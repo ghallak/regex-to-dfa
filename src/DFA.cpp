d6eb3cff9dce0aa363c86a88203a2a4da95fad43
Gaith Hallak <gaithhallak@gmail.com>
2019-07-11 01:23:39 +0300

    use struct instead of std::pair

--- src/DFA.cpp
+++ src/DFA.cpp
@@ -7,10 +7,21 @@
 #include <utility>
 #include <vector>
 
+namespace {
+struct DState {
+  DState(std::unique_ptr<DFA::State> sp,
+         const std::unordered_set<std::size_t>& fp)
+      : state(std::move(sp)), followpos(fp) {}
+  DState(std::unique_ptr<DFA::State> sp, std::unordered_set<std::size_t>&& fp)
+      : state(std::move(sp)), followpos(std::move(fp)) {}
+
+  std::unique_ptr<DFA::State> state;
+  std::unordered_set<std::size_t> followpos;
+};
+}  // namespace
+
 DFA::DFA(const RegexTree& tree) {
-  std::vector<
-      std::pair<std::unique_ptr<State>, std::unordered_set<std::size_t>>>
-      dstates;
+  std::vector<DState> dstates;
 
   // add the initial state
   dstates.emplace_back(std::make_unique<State>(), tree.FirstPosRoot());
@@ -19,38 +30,39 @@ DFA::DFA(const RegexTree& tree) {
 
   for (std::size_t i = 0; i < dstates.size(); ++i) {
     if (std::any_of(
-            dstates[i].second.cbegin(), dstates[i].second.cend(),
+            dstates[i].followpos.cbegin(), dstates[i].followpos.cend(),
             [sz = alphabet.size()](auto next_pos) { return next_pos == sz; })) {
-      dstates[i].first->MakeAcceptState();
+      dstates[i].state->MakeAcceptState();
     }
 
     for (auto character : alphabet) {
       std::unordered_set<std::size_t> new_next_positions;
-      for (auto next_pos : dstates[i].second) {
+
+      for (auto next_pos : dstates[i].followpos) {
         if (tree.CharAtPos(next_pos) == character) {
-          auto s = tree.FollowPos(next_pos);
-          new_next_positions.insert(s.cbegin(), s.cend());
+          auto followpos = tree.FollowPos(next_pos);
+          new_next_positions.insert(followpos.cbegin(), followpos.cend());
         }
       }
 
-      //  // TODO: how can this happen?
-      //  if (new_next_positions.empty()) continue;
+      if (new_next_positions.empty()) continue;
 
       auto old_dstate = std::find_if(
           dstates.cbegin(), dstates.cend(),
           [& nnp = std::as_const(new_next_positions)](const auto& dstate) {
-            return nnp == dstate.second;
+            return nnp == dstate.followpos;
           });
 
       if (old_dstate == dstates.end()) {
-        dstates.emplace_back(std::make_unique<State>(), new_next_positions);
-        dstates[i].first->AddTransition(dstates.back().first.get(), character);
+        dstates.emplace_back(std::make_unique<State>(),
+                             std::move(new_next_positions));
+        dstates[i].state->AddTransition(dstates.back().state.get(), character);
       } else {
-        dstates[i].first->AddTransition(old_dstate->first.get(), character);
+        dstates[i].state->AddTransition(old_dstate->state.get(), character);
       }
     }
   }
 
   // move the states ownership to the DFA
-  for (auto& state : dstates) states.emplace_back(std::move(state.first));
+  for (auto& dstate : dstates) states.emplace_back(std::move(dstate.state));
 }
